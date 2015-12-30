9a03a993e1828d19b3bb58088acfb2b6332730a1
David Itkin <ditkin@Nair.local>
2015-12-30 14:16:59 -0500

    make backup dirs for vim

--- fresh.sh
+++ fresh.sh
@@ -1,5 +1,6 @@
 git clone git@github.com:ditkin/dotfiles.git ~/dotfiles
 git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/vim/bundle/Vundle.vim
+mkdir -p ~/dotfiles/vim/backup_files ~/dotfiles/vim/swap_files ~/dotfiles/vim/undo_files
 mkdir -p ~/dotfiles/vim/autoload ~/dotfiles/vim/bundle && \
 curl -LSso ~/dotfiles/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/zsh-users/antigen.git ~/dotfiles/antigen
