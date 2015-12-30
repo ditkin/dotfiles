1ceaf06308f5d82722e1983d7d7b4c822114f733
David Itkin <ditkin@Nair.local>
2015-12-30 14:13:10 -0500

    manually get pathogen

--- fresh.sh
+++ fresh.sh
@@ -1,5 +1,7 @@
 git clone git@github.com:ditkin/dotfiles.git ~/dotfiles
 git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/vim/bundle/Vundle.vim
+mkdir -p ~/dotfiles/vim/autoload ~/dotfiles/vim/bundle && \
+curl -LSso ~/dotfiles/vim/autoload/pathogen.vim https://tpo.po/pathogen.vim
 git clone https://github.com/zsh-users/antigen.git ~/dotfiles/antigen
 git clone git@github.com:ditkin/scm_breeze.git ~/dotfiles/scm_breeze
 sudo easy_install pip
